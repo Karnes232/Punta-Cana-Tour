bb66c811292a3b3527b3979f1727114a33e99f19
James Karnes <karnes.james@gmail.com>
2023-08-14 18:54:30 -0400

    adding layout to thankyou page

--- src/pages/contact/thankyou.js
+++ src/pages/contact/thankyou.js
@@ -1,12 +1,51 @@
+import { graphql } from "gatsby";
 import React, { useEffect, useState } from "react";
 
-const Thankyou = () => {
+const Thankyou = ({data}) => {
   const [name, setName] = useState("")
   useEffect(() => {
     const searchParams = new URLSearchParams(document.location.search)
     setName(searchParams.get("name"))
   }, [])
-  return <div>{name}</div>;
+  return (
+    <Layout
+      logo={data.allContentfulLayout.edges[0].node.logo.gatsbyImage}
+      footerBackground={
+        data.allContentfulLayout.edges[0].node.footerBackground.url
+      }
+      facebook={data.allContentfulLayout.edges[0].node.facebook}
+      instagram={data.allContentfulLayout.edges[0].node.instagram}
+      email={data.allContentfulLayout.edges[0].node.email}
+      gImage={
+        data.allContentfulLayout.edges[0].node.footerBackground.gatsbyImage
+      }
+    >
+    {name}
+    </Layout>
+  )
 };
+
+export const query = graphql`
+  query MyQuery {
+    allContentfulLayout {
+      edges {
+        node {
+          logo {
+            gatsbyImage(width: 150, formats: WEBP)
+          }
+          footerBackground {
+            url
+            gatsbyImage(width: 1920, formats: WEBP)
+          }
+          email
+          facebook
+          instagram
+        }
+      }
+    }
+  }
+`;
+
+export const Head = () => <title>Thank You Page</title>;
 
 export default Thankyou;
