793c3927af1aa37c7249c4c18d7ce0e8e1485c5c
James Karnes <karnes.james@gmail.com>
2023-09-01 18:43:41 -0400

    adding cart component

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import CartComponent from '../components/CartComponent/CartComponent'
+import { CartProvider } from '../context/cart'
 
 const Cart = () => {
   return (
+    <CartProvider>
     <div>
         <CartComponent />
-    </div>
+    </div></CartProvider>
   )
 }
 
