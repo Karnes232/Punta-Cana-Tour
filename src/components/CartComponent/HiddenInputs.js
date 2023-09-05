99c3c98a04f24570f4c5de51b9a7a3960446898c
James Karnes <karnes.james@gmail.com>
2023-09-05 11:28:42 -0400

    added dash after name on hidden inputs for better readability on netlify

--- src/components/CartComponent/HiddenInputs.js
+++ src/components/CartComponent/HiddenInputs.js
@@ -6,14 +6,14 @@ const HiddenInputs = () => {
   console.log(cartItems);
   return (
     <div className="hidden">
-      <input type="hidden" name="Tour 1" value={cartItems[0]?.name || "None"} />
-      <input type="hidden" name="Pax 1" value={cartItems[0]?.quantity || 0} />
-      <input type="hidden" name="Tour 2" value={cartItems[1]?.name || "None"} />
-      <input type="hidden" name="Pax 2" value={cartItems[1]?.quantity || 0} />
-      <input type="hidden" name="Tour 3" value={cartItems[2]?.name || "None"} />
-      <input type="hidden" name="Pax 3" value={cartItems[2]?.quantity || 0} />
-      <input type="hidden" name="Tour 4" value={cartItems[3]?.name || "None"} />
-      <input type="hidden" name="Pax 4" value={cartItems[3]?.quantity || 0} />
+      <input type="hidden" name="Tour 1 -" value={cartItems[0]?.name || "None"} />
+      <input type="hidden" name="Pax 1 -" value={cartItems[0]?.quantity || 0} />
+      <input type="hidden" name="Tour 2 -" value={cartItems[1]?.name || "None"} />
+      <input type="hidden" name="Pax 2 -" value={cartItems[1]?.quantity || 0} />
+      <input type="hidden" name="Tour 3 -" value={cartItems[2]?.name || "None"} />
+      <input type="hidden" name="Pax 3 -" value={cartItems[2]?.quantity || 0} />
+      <input type="hidden" name="Tour 4 -" value={cartItems[3]?.name || "None"} />
+      <input type="hidden" name="Pax 4 -" value={cartItems[3]?.quantity || 0} />
     </div>
   );
 };
