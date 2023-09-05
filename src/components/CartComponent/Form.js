722e75727931212b10ef4f822032276c2942592e
James Karnes <karnes.james@gmail.com>
2023-09-05 15:34:18 -0400

    testing button to clear cart

--- src/components/CartComponent/Form.js
+++ src/components/CartComponent/Form.js
@@ -9,21 +9,21 @@ const Form = () => {
   const [name, setName] = useState("");
   const { cartItems, addToCart, removeFromCart, clearCart, getCartTotal } =
     useContext(CartContext);
-    const handleSubmit = (event) => {
-        event.preventDefault();
-      
-        const myForm = event.target;
-        const formData = new FormData(myForm);
-        
-        fetch("/", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: new URLSearchParams(formData).toString(),
-        })
-          .then(() => console.log("Form successfully submitted"))
-          .catch((error) => alert(error));
-      };
-      
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => console.log("Form successfully submitted"))
+      .catch((error) => alert(error));
+  };
+
   return (
     <form
       name="cart"
