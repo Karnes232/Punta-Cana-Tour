3075f8cc636ab429ee1e62f879a78fc0b80701c2
James Karnes <karnes.james@gmail.com>
2023-09-06 09:28:51 -0400

    added thank you back after form submission

--- src/pages/contact/thankyou.js
+++ src/pages/contact/thankyou.js
@@ -2,6 +2,7 @@ import { graphql } from "gatsby";
 import React, { useEffect, useState } from "react";
 import Layout from "../../components/layout";
 import Seo from "../../components/seo";
+import CartLayout from "../../components/cartLayout";
 
 const Thankyou = ({ data }) => {
   const [name, setName] = useState("");
@@ -10,7 +11,7 @@ const Thankyou = ({ data }) => {
     setName(searchParams.get("name"));
   }, []);
   return (
-    <Layout
+    <CartLayout
       logo={data.allContentfulLayout.edges[0].node.logo.gatsbyImage}
       footerBackground={
         data.allContentfulLayout.edges[0].node.footerBackground.url
@@ -22,8 +23,32 @@ const Thankyou = ({ data }) => {
         data.allContentfulLayout.edges[0].node.footerBackground.gatsbyImage
       }
     >
-      {name}
-    </Layout>
+    <main className>
+        <div className="flex flex-col items-center max-w-xs xl:max-w-sm mx-auto">
+          <div className>
+            <div className="flex flex-col justify-center items-center text-slate-600 ">
+              <div className="text-2xl xl:text-4xl font-serif text-center mt-6">
+                Thank you {name}, our team will reach out to you shortly!
+              </div>
+
+              <div className="text-center text-sm xl:text-base mt-2 xl:mt-6">
+                Please feel free to{" "}
+                <a
+                  href={`mailto:${data.allContentfulLayout.edges[0].node.email}`}
+                  aria-label="Gmail"
+                  rel="noreferrer"
+                  className="underline"
+                >
+                  contact us
+                </a>{" "}
+                with any questions or concerns.
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+
+    </CartLayout>
   );
 };
 
