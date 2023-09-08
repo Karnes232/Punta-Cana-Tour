93d9aeccaba703dd96aaf499eda2c2b7a55dc90d
James Karnes <karnes.james@gmail.com>
2023-09-08 11:39:08 -0400

    fixing error on datePicker

--- src/components/CartComponent/DatePickerComponent.js
+++ src/components/CartComponent/DatePickerComponent.js
@@ -1,11 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Datepicker from "react-tailwindcss-datepicker";
 
 const DatePickerComponent = () => {
+  const [date, setDate] = useState(undefined);
   const [value, setValue] = useState({
-    startDate: new Date(),
+    startDate: date,
     endDate: null,
   });
+  useEffect(() => {
+    setDate(new Date());
+  }, []);
 
   const handleValueChange = (newValue) => {
     setValue(newValue);
@@ -17,7 +21,7 @@ const DatePickerComponent = () => {
         asSingle={true}
         useRange={false}
         placeholder={"Select Date"}
-        minDate={new Date()}
+        minDate={date}
         value={value}
         onChange={handleValueChange}
         inputClassName="relative transition-all duration-300 py-2.5 pl-4 pr-14 w-full border-gray-300  rounded-lg tracking-wide font-light text-sm placeholder-gray-400 bg-white focus:ring disabled:opacity-40 disabled:cursor-not-allowed focus:border-blue-500 focus:ring-blue-500/20"
