1ff626741bf2c13ec8078db87b6da769204a313d
James Karnes <karnes.james@gmail.com>
2023-09-08 14:06:45 -0400

    updated date format in copyright for react minified error

--- src/components/FooterComponent/Copyright.js
+++ src/components/FooterComponent/Copyright.js
@@ -1,12 +1,16 @@
 import { Link } from "gatsby";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Copyright = () => {
+  const [date, setDate] = useState(undefined);
+  useEffect(() => {
+    setDate(new Date().getFullYear());
+  }, []);
   return (
     <div className="mx-8 flex flex-col justify-between py-4 md:flex-row md:items-center md:mx-auto">
       <Link to="/">
         <p className="footerSitemap font-medium tracking-wider">
-          &copy; {new Date().getFullYear()} ThrillQuest Punta Cana
+          &copy; {date} ThrillQuest Punta Cana
         </p>
       </Link>
     </div>
