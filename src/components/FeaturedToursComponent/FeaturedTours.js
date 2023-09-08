48a98d2834c3ce77a2c4cb99a31857ea54631289
James Karnes <karnes.james@gmail.com>
2023-09-08 15:25:32 -0400

    added small arrow on featured tour button

--- src/components/FeaturedToursComponent/FeaturedTours.js
+++ src/components/FeaturedToursComponent/FeaturedTours.js
@@ -1,6 +1,7 @@
 import React from "react";
 import TourCard from "../TourCardComponent/TourCard";
 import { Link } from "gatsby";
+import { RiArrowRightSLine } from "react-icons/ri";
 
 const FeaturedTours = ({ tours }) => {
   let tourList = tours.splice(0, 6).sort(() => Math.random() - 0.5);
@@ -16,8 +17,8 @@ const FeaturedTours = ({ tours }) => {
         ))}
       </div>
       <div className="flex flex-col justify-center items-center max-w-5xl xl:max-w-6xl mx-auto">
-        <Link to="/tours/" className="no-underline text-2xl font-medium bg-primary-color text-secondary-color px-4 py-2 my-3 rounded-xl">
-          More Tours
+        <Link to="/tours/" className="flex items-center justify-between no-underline text-2xl font-medium bg-primary-color text-secondary-color px-4 py-2 my-3 rounded-xl hover:opacity-70">
+          More Tours <RiArrowRightSLine className="ml-2" />
         </Link>
       </div>
     </div>
