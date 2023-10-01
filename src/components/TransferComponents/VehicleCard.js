2515f0573380079fbe9ce0c027e70bc7b8dd380c
James Karnes <karnes.james@gmail.com>
2023-10-01 17:59:56 -0400

    showing form on transfer page

--- src/components/TransferComponents/VehicleCard.js
+++ src/components/TransferComponents/VehicleCard.js
@@ -7,7 +7,7 @@ import TransferPayPalWrapper from "../PayPalButtonWrapper/TransferPayPalWrapper"
 
 const VehicleCard = ({ vehicle, formData }) => {
   const image = getImage(vehicle.vehiclePhoto.gatsbyImage);
-  let price = null;
+  let price = 1;
   if (formData.zone === "1") {
     price = vehicle.zone1Price;
   }
