94d787157fef7425da0180d81ca4899a59ae7697
James Karnes <karnes.james@gmail.com>
2023-11-01 12:14:54 -0400

    forgot to add useEffect

--- src/components/TravelAgentComponents/CartComponents/Form.js
+++ src/components/TravelAgentComponents/CartComponents/Form.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { TravelAgentCartContext } from "../../../context/travelAgentCart";
 import CartComponent from "../CartComponents/CartComponent";
 import ContactInfo from "./ContactInfo";
