77815398895f7bb107e89d9cfcb3ce0666da144c
James Karnes <karnes.james@gmail.com>
2023-11-07 21:36:56 -0400

    adjust height of navbar

--- src/components/TravelAgentComponents/Header.js
+++ src/components/TravelAgentComponents/Header.js
@@ -6,7 +6,7 @@ function Header({ logo }) {
   return (
     // mb-[50vh] md:mb-[40vh] lg:mb-[50vh]
     <>
-      <nav className="bg-white w-screen z-50 h-[10vh] md:h-36 fixed top-0 shadow-md">
+      <nav className="bg-white w-screen z-50 h-24 md:h-36 fixed top-0 shadow-md">
         <div className="flex items-center justify-between bg-transparent max-w-6xl mx-5 md:mx-10 lg:mx-8 xl:mx-auto">
           <Logo logo={logo} />
           {/* 
