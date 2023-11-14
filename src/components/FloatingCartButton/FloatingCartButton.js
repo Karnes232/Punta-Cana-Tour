dacb7cec0b8e274fd070d9c0f662a91aa398542e
James Karnes <karnes.james@gmail.com>
2023-11-14 16:40:40 -0400

    testing cta button on left side

--- src/components/FloatingCartButton/FloatingCartButton.js
+++ src/components/FloatingCartButton/FloatingCartButton.js
@@ -9,7 +9,7 @@ const FloatingCartButton = () => {
     <>
       {cartItems.length !== 0 ? (
         <Link to="/cart/" className="no-underline">
-          <button className="fixed z-50 flex px-2 justify-between items-center bottom-24 right-6 xl:top-10 xl:right-10 rounded-full h-14 w-14 bg-primary-color text-secondary-color  font-bold">
+          <button className="fixed z-50 flex px-2 justify-between items-center bottom-6 left-6 xl:top-10 xl:right-10 rounded-full h-14 w-14 bg-primary-color text-secondary-color  font-bold">
             <AiOutlineShoppingCart size={25} />
             {cartItems.length}
           </button>
