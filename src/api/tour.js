1ecdc334ddaffe57f337ababbfbbc7f1a648bf90
James Karnes <karnes.james@gmail.com>
2023-11-16 17:58:13 -0400

    added handlebars and moving email after tour bought to template

--- src/api/tour.js
+++ src/api/tour.js
@@ -1,4 +1,6 @@
 const nodemailer = require("nodemailer");
+const handlebars = require("handlebars");
+const fs = require("fs");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -7,25 +9,39 @@ const transporter = nodemailer.createTransport({
     pass: `${process.env.EMAIL_PASSWORD_NEW}`,
   },
 });
+
+const loadTemplate = (templateName, context) => {
+  const templatePath = `src/views/${templateName}.handlebars`;
+  const source = fs.readFileSync(templatePath, "utf8");
+  const template = handlebars.compile(source);
+  return template(context);
+};
+
 export default function handler(req, res) {
   const clientName = req.body.clientName;
   const clientEmail = req.body.clientEmail;
   const deposit = req.body.deposit;
-  console.log(req.body);
+
+  const templateName = "tour";
+  const emailContext = {
+    /* Any dynamic content to be inserted into the template */
+    name: clientName,
+    // Add more data here based on your template's placeholders
+  };
+  const emailHTML = loadTemplate(templateName, emailContext);
   let mailDetails = {
     from: `${process.env.EMAIL_USER_NEW}`,
     to: clientEmail,
     subject: "Punta Cana Tour Store",
     text: `${clientName} have a deposit of ${deposit}`,
-    html: `<h1>${clientName}</h1>
-        <p>${clientName} have a deposit of ${deposit}</p>`,
+    html: emailHTML,
   };
 
   transporter.sendMail(mailDetails, function (err, data) {
     if (err) {
       console.log(err);
     } else {
-      console.log("Email sent successfully");
+      console.log(data);
     }
   });
 
