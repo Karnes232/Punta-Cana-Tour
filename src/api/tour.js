fdc256655eb0de437fc15169b9b0255c9270195f
James Karnes <karnes.james@gmail.com>
2023-11-17 14:39:06 -0400

    removed unnecessary packages

--- src/api/tour.js
+++ src/api/tour.js
@@ -1,9 +1,6 @@
 const nodemailer = require("nodemailer");
-const handlebars = require("handlebars");
-const fs = require("fs");
-import * as path from "path";
+
 import { Head } from "../components/Email/Head";
-import { CssReset } from "../components/Email/CssReset";
 import { Css } from "../components/Email/Css";
 import { Body } from "../components/Email/Body";
 import { HtmlHeader } from "../components/Email/HtmlHeader";
@@ -16,30 +13,15 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-// const loadTemplate = (templateName, context) => {
-//   let templatePath = ""
-//   if (process.env.NODE_ENV === 'production') {
-//     templatePath = productionEmail
-//     const template = handlebars.compile(templatePath);
-//     return template(context);
-//   } else {
-//     templatePath = `src/views/${templateName}.handlebars`;
-//     const source = fs.readFileSync(templatePath, "utf8");
-//     const template = handlebars.compile(source);
-//     return template(context);
-//   }
-//   // const source = fs.readFileSync(templatePath, "utf8");
-//   // const template = handlebars.compile(source);
-//   // return template(context);
-// };
-
 export default function handler(req, res) {
   const clientName = req.body.clientName;
   const clientEmail = req.body.clientEmail;
   const deposit = req.body.deposit;
   console.log(req.body.tourList[0].tour);
 
-  const htmlString = `${Head}${Css}${HtmlHeader}${Body(clientName)}${HtmlFooter}`;
+  const htmlString = `${Head}${Css}${HtmlHeader}${Body(
+    clientName,
+  )}${HtmlFooter}`;
 
   let mailDetails = {
     from: `${process.env.EMAIL_USER_NEW}`,
