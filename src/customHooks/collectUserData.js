86bdbd24b4f28b37105a16a0f40d6c8ac9f6f860
James Karnes <karnes.james@gmail.com>
2023-11-18 16:20:04 -0400

    changed name of where the reservation are stored in database to a date format

--- src/customHooks/collectUserData.js
+++ src/customHooks/collectUserData.js
@@ -2,18 +2,27 @@ import { doc, setDoc } from "firebase/firestore";
 import { db } from "../config/firebase";
 
 const collectUserData = async (formData) => {
+  const createdAt = new Date();
   const formDataObj = {};
   formData.forEach((value, key) => (formDataObj[key] = value));
 
-  await setDoc(doc(db, "reservationsClientes", formDataObj.email), {
-    email: formDataObj.email,
-    name: formDataObj.name,
-    phone: formDataObj.phone,
-    hotel: formDataObj.location,
-    excursionDate: formDataObj.Date,
-    message: formDataObj.additional,
-    formInfo: formDataObj,
-  });
+  await setDoc(
+    doc(
+      db,
+      "reservationsClientes",
+      createdAt.toString().split(" ").slice(1).join(" "),
+    ),
+    {
+      email: formDataObj.email,
+      name: formDataObj.name,
+      phone: formDataObj.phone,
+      hotel: formDataObj.location,
+      excursionDate: formDataObj.Date,
+      message: formDataObj.additional,
+      formInfo: formDataObj,
+      createdAt: createdAt,
+    },
+  );
 };
 
 export default collectUserData;
