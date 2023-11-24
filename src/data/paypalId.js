faa20ba6e331e86217450f9d807ce37168e9d9b3
James Karnes <karnes.james@gmail.com>
2023-11-24 14:42:59 -0400

    paypal live

--- src/data/paypalId.js
+++ src/data/paypalId.js
@@ -1,6 +1,6 @@
 const liveId =
-  "AQEgC9JKIvPEyauVBaX6Kxv1cQwaiylZ_Cbzu29ORf_ChBYZeM5FwGUSd4hCXGBMFBYAQLN2Vpez50lx";
+  "AZ77ez0KHLZxKYuNMK35zAGRMxiiXntV0yB545r18Ub0ac518N1AxHAmTxDh8QOG3h-FLqj_Lg-srlOY";
 const sandBox =
   "AWKpOxlq063t4e3-YvGIHBWohFbzZ_o0Y1M2juHc6EaAr5iK_UfOAEKb_YxhdpRvC5uu_Sj444MyUzmZ";
-
-export const paypalId = sandBox
+const jamesId = "AaPiNuBE-3bjn86CtDSbnbs5nnaeQ-vNhBk48DdMwZ0vsUYGVuE1_38burybKxv_Qn78gXQYUSKf1UG0"
+export const paypalId = liveId
