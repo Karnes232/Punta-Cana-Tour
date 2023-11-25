5a7780d00eac4c60f348b789eadce0e2e06762ec
James Karnes <karnes.james@gmail.com>
2023-11-25 17:02:30 -0400

    submiting form on reserverations to netlify

--- src/components/PayPalButtonWrapper/CartPayPalWrapper.js
+++ src/components/PayPalButtonWrapper/CartPayPalWrapper.js
@@ -29,6 +29,13 @@ const CartPayPalWrapper = ({
     setHost(window.location.origin);
   }, [currency, showSpinner]);
   let redirectHref = `${host}/payment/thankyou/?name=${clientName}&totalPrice=${totalCost}`;
+
+  function getFormData(object) {
+    const formData = new FormData();
+    Object.keys(object).forEach((key) => formData.append(key, object[key]));
+    return formData;
+  }
+
   return (
     <>
       {showSpinner && isPending && <div className="spinner" />}
@@ -61,22 +68,33 @@ const CartPayPalWrapper = ({
           return actions.order.capture().then(function (details) {
             const deposit = details.purchase_units[0].amount.value;
             const depositString = `&deposit=${deposit}`;
-            console.log("Appoved");
-            axios.post("/api/tour", {
-              clientName: clientName,
-              clientEmail: clientEmail,
-              deposit: deposit,
-              totalPrice: totalCost,
-              tourList: tourList,
-            });
-            collectUserDataPayPal(
-              details,
-              redirectHref,
-              depositString,
-              totalCost,
-              formData,
-              clearCart,
-            );
+
+            const dataFromForm = getFormData(formData);
+            fetch("/", {
+              method: "POST",
+              headers: { "Content-Type": "application/x-www-form-urlencoded" },
+              body: new URLSearchParams(dataFromForm).toString(),
+            })
+              .then(() => {
+                console.log("Form successfully submitted");
+                axios.post("/api/tour", {
+                  clientName: clientName,
+                  clientEmail: clientEmail,
+                  deposit: deposit,
+                  totalPrice: totalCost,
+                  tourList: tourList,
+                });
+                collectUserDataPayPal(
+                  details,
+                  redirectHref,
+                  depositString,
+                  totalCost,
+                  formData,
+                  clearCart,
+                );
+              })
+              .catch((error) => alert(error));
+
             // window.location.href = redirectHref + depositString;
           });
         }}
