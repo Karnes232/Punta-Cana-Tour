10ef2d1548d03702031d8f96747332cf2e988c28
James Karnes <karnes.james@gmail.com>
2023-11-27 16:57:48 -0400

    made paypal live

--- src/data/paypalId.js
+++ src/data/paypalId.js
@@ -4,4 +4,4 @@ const sandBox =
   "AWKpOxlq063t4e3-YvGIHBWohFbzZ_o0Y1M2juHc6EaAr5iK_UfOAEKb_YxhdpRvC5uu_Sj444MyUzmZ";
 const jamesId =
   "AaPiNuBE-3bjn86CtDSbnbs5nnaeQ-vNhBk48DdMwZ0vsUYGVuE1_38burybKxv_Qn78gXQYUSKf1UG0";
-export const paypalId = sandBox;
+export const paypalId = liveId;
