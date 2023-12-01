7ada42af15cac05098dc03d2976ae7d22db526ed
James Karnes <karnes.james@gmail.com>
2023-12-01 14:33:14 -0400

    changed to paypal sandbox for testing

--- src/data/paypalId.js
+++ src/data/paypalId.js
@@ -2,6 +2,5 @@ const liveId =
   "AZ77ez0KHLZxKYuNMK35zAGRMxiiXntV0yB545r18Ub0ac518N1AxHAmTxDh8QOG3h-FLqj_Lg-srlOY";
 const sandBox =
   "AWKpOxlq063t4e3-YvGIHBWohFbzZ_o0Y1M2juHc6EaAr5iK_UfOAEKb_YxhdpRvC5uu_Sj444MyUzmZ";
-const jamesId =
-  "AaPiNuBE-3bjn86CtDSbnbs5nnaeQ-vNhBk48DdMwZ0vsUYGVuE1_38burybKxv_Qn78gXQYUSKf1UG0";
-export const paypalId = liveId;
+
+export const paypalId = sandBox;
