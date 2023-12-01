e6b0f39f97c6d86071430df17097fd039d56b927
James Karnes <karnes.james@gmail.com>
2023-12-01 17:11:05 -0400

    adding hidden inputs to create form on netlify

--- src/components/CartComponent/HiddenInputs.js
+++ src/components/CartComponent/HiddenInputs.js
@@ -25,6 +25,18 @@ const HiddenInputs = ({ formData, setFormData }) => {
       />
       <input
         type="hidden"
+        name="PickUpTime1"
+        value={`- ` + formData.PickUp1 || 0}
+        onChange={handleChange}
+      />
+      <input
+        type="hidden"
+        name="Date1"
+        value={`- ` + formData.Date1 || 0}
+        onChange={handleChange}
+      />
+      <input
+        type="hidden"
         name="Tour2"
         value={`- ` + cartItems[1]?.name || "None"}
         onChange={handleChange}
@@ -33,6 +45,18 @@ const HiddenInputs = ({ formData, setFormData }) => {
         type="hidden"
         name="Pax2"
         value={`- ` + cartItems[1]?.quantity || 0}
+        onChange={handleChange}
+      />
+      <input
+        type="hidden"
+        name="PickUpTime2"
+        value={`- ` + formData.PickUp2 || 0}
+        onChange={handleChange}
+      />
+      <input
+        type="hidden"
+        name="Date2"
+        value={`- ` + formData.Date2 || 0}
         onChange={handleChange}
       />
       <input
@@ -49,6 +73,18 @@ const HiddenInputs = ({ formData, setFormData }) => {
       />
       <input
         type="hidden"
+        name="PickUpTime3"
+        value={`- ` + formData.PickUp3 || 0}
+        onChange={handleChange}
+      />
+      <input
+        type="hidden"
+        name="Date3"
+        value={`- ` + formData.Date3 || 0}
+        onChange={handleChange}
+      />
+      <input
+        type="hidden"
         name="Tour4"
         value={`- ` + cartItems[3]?.name || "None"}
         onChange={handleChange}
@@ -57,6 +93,18 @@ const HiddenInputs = ({ formData, setFormData }) => {
         type="hidden"
         name="Pax4"
         value={`- ` + cartItems[3]?.quantity || 0}
+        onChange={handleChange}
+      />
+      <input
+        type="hidden"
+        name="PickUpTime4"
+        value={`- ` + formData.PickUp4 || 0}
+        onChange={handleChange}
+      />
+      <input
+        type="hidden"
+        name="Date4"
+        value={`- ` + formData.Date4 || 0}
         onChange={handleChange}
       />
     </div>
